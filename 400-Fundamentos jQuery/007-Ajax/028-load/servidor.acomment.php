f4370562f475701ab7a0fafe320d7e9683d528f0
jocarsa <info@josevicentecarratala.com>
2023-06-01 15:01:25 +0200

    Actualizar servidor.acomment.php

---
+++ 400-Fundamentos jQuery/007-Ajax/028-load/servidor.acomment.php
@@ -0,0 +1,6 @@
+ El script del servidor únicamente contendrá una sencilla instrucción de impresión en pantalla, que escribirá algo así como " hola desde Php "
+
+Si ejecutamos el conjunto de estos dos archivos, podremos comprobar cómo, al realizar una petición sincrónica desde el primer archivo, pulsando el botón, dentro del elemento, cuyo identificador es " contenedor ", podremos introducir el contenido que nos sirve el script en lenguaje de Php.
+
+De esta forma, vemos cómo podemos conectar tanto el cliente con el servidor de una forma terriblemente sencilla, utilizando, en este caso, una única instrucción llamada nueva.
+
