6d4236b4b5bcee7c10233385010d7879d1f0926e
jocarsa <info@josevicentecarratala.com>
2023-06-01 22:54:56 +0200

    Actualizar editor.acomment.js

---
+++ 401-Ejercicio jQuery/401-Proyecto-Bases del editor/js/editor.acomment.js
@@ -0,0 +1,12 @@
+
+El archivo de código, por el momento, se encontrará vacío.
+
+En esta versión del proyecto se ha creado un archivo de código, aunque vacío, con el objetivo de dejar clara, cuál será la estructura del proyecto, y en próximas interacciones, poco a poco iremos introduciendo nuevo contenido.
+
+Evidentemente es muy importante diferenciar el contenido de este archivo, con respecto al contenido del archivo de importación de jQuery.
+
+Mientras que el archivo de jQuery consiste en la librería que contiene todos los bloques de código que vamos a utilizar para aumentar nuestra productividad, este archivo con el que vamos a trabajar es aquel en el que introducimos nuestro propio código escrito en JavaScript.
+
+Si bien hasta el momento hemos estado trabajando, los ejercicios individuales, en archivos HTML, que contenían también, de forma interna, el código JavaScript, esto se ha realizado de esta forma, para no tener que duplicar los archivos, y poder trabajar con un único archivo dentro de cada uno de los ejercicios de la parte principal e inicial de este curso.
+
+Sin embargo, ahora, en esta ocasión, dado que estamos realizando un ejercicio práctico, más parecido a un proyecto real, lo que queremos es trabajar de forma organizada y estructurada, y por esta razón diferenciamos , en dos archivos separados, la estructura, en HTML, de la interactividad, en formato JavaScript.
